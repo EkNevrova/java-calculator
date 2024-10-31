1c0c0da3dc49e702adb452372837278cfb9b7b09
Ekaterina Nevrova <ek.nevrova@mail.ru>
2024-10-31 18:42:11 +0300

    Change classes

--- src/main/java/ru/nevrova/calcapp/_main/_Main.java
+++ src/main/java/ru/nevrova/calcapp/_main/_Main.java
@@ -6,18 +6,19 @@ import ru.nevrova.calcapp.util.Calculator;
 import java.util.Scanner;
 
 public class _Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
+        //int resultAdding = Calculator.adding(num1, num2);
+        //int resultOperation = Calculator.operation(num1, num2, "+");
+        ResultWriterService.resultWriterService(num1, num2, Calculator.operation(num1, num2, "+"), "Операция сложения -");
+        ResultWriterService.resultWriterService(num1, num2, Calculator.operation(num1, num2, "-"), "Операция вычитания -");
+        ResultWriterService.resultWriterService(num1, num2, Calculator.operation(num1, num2, "*"), "Операция умножения -");
 
-        int resultAdding = Calculator.adding(num1, num2);
-        ResultWriterService.resultWriterService(num1, num2, resultAdding, "Операция сложения -");
-
-        int resultSubtraction = Calculator.subtraction(num1, num2);
-        ResultWriterService.resultWriterService(num1, num2, resultSubtraction, "Операция вычитания -");
-
-        int resultMultiplication = Calculator.multiplication(num1, num2);
-        ResultWriterService.resultWriterService(num1, num2, resultMultiplication, "Операция умножения -");
+        //int resultSubtraction = Calculator.subtraction(num1, num2);
+        //ResultWriterService.resultWriterService(num1, num2, resultSubtraction, "Операция вычитания -");
+        //int resultMultiplication = Calculator.multiplication(num1, num2);
+        //ResultWriterService.resultWriterService(num1, num2, resultMultiplication, "Операция умножения -");
     }
 }
--- src/main/java/ru/nevrova/calcapp/util/Calculator.java
+++ src/main/java/ru/nevrova/calcapp/util/Calculator.java
@@ -1,18 +1,29 @@
 package ru.nevrova.calcapp.util;
 
 public class Calculator {
-    public static int adding ( int x, int y) {
-        int result = x + y;
-        return result;
-    }
-
-    public static int subtraction ( int x, int y) {
-        int result = x - y;
-        return result;
-    }
-
-    public static int multiplication ( int x, int y) {
-        int result = x * y;
+//    public static int adding ( int x, int y) {
+//        return x + y;
+//    }
+//
+//    public static int subtraction ( int x, int y) {
+//        return x - y;
+//    }
+//
+//    public static int multiplication ( int x, int y) {
+//        return x * y;
+//    }
+    public static <String> int operation(int num1, int num2, String symbol) {
+        int result;
+        switch (symbol) {
+            case "+":  result = num1 + num2;
+                break;
+            case  "-": result = num1 - num2;
+                break;
+            case  "*": result = num1 * num2;
+                break;
+            default:
+                break;
+        }
         return result;
     }
 }
